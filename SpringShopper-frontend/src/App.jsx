b92cea628b3da8d80800888744264d1ba28f8245
Sarthak Mohaniraj <mohanirajsarthak@gmail.com>
2025-12-08 06:11:17 +0530

    Created components to display products

--- SpringShopper-frontend/src/App.jsx
+++ SpringShopper-frontend/src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import Products from './components/Products'
 
 function App() {
 
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
-     Hello world!
-  </h1>
+     <Products/>
     </>
   )
 }
