9f9cdcf2a5a5a6f5bb2bb506610f75c3b2073d49
Sarthak Mohaniraj <mohanirajsarthak@gmail.com>
2025-12-17 01:09:07 +0530

    Started admin panel basic analytics data return

--- SpringShopper-frontend/src/components/PrivateRoute.jsx
+++ SpringShopper-frontend/src/components/PrivateRoute.jsx
@@ -2,13 +2,33 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
-const PrivateRoute = ({publicPage = false}) => {
-    const {user} = useSelector((state) => state.auth)
-     if (publicPage) {
+const PrivateRoute = ({ publicPage = false, adminOnly = false }) => {
+    const { user } = useSelector((state) => state.auth);
+    const isAdmin = user && user?.roles?.includes("ROLE_ADMIN");
+    const isSeller = user && user?.roles.includes("ROLE_SELLER");
+    const location = useLocation();
+
+    if (publicPage) {
         return user ? <Navigate to="/" /> : <Outlet />
     }
 
-    return user ? <Outlet /> : <Navigate to="/login" replace />;
+    if (adminOnly) {
+        if (isSeller && !isAdmin) {
+            const sellerAllowedPaths = ["/admin/orders", "/admin/products"];
+            const sellerAllowed = sellerAllowedPaths.some(path => 
+                location.pathname.startsWith(path)
+            );
+            if (!sellerAllowed) {
+                return <Navigate to="/" replace />
+            }
+        }
+    }
+
+    if (!isAdmin && !isSeller) {
+        return <Navigate to="/"/>
+    }
+    
+    return user ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default PrivateRoute
---
+++ src/main/java/SpringBootEcom/service/AnalyticsService.java
@@ -0,0 +1,7 @@
+package SpringBootEcom.service;
+
+import SpringBootEcom.payload.AnalyticsResponse;
+
+public interface AnalyticsService {
+    AnalyticsResponse getAnalyticsData();
+}
